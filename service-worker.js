adb0b2b006aac6a9de2132a5ebcea1f5a9015e23
BFSBaynes <55phc74t5c@privaterelay.appleid.com>
2026-03-24 11:06:56 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "door-takeoff-v2.1.1"; // Increment this to force an update
+const CACHE_NAME = "door-takeoff-v2.1.5"; // Increment this to force an update
 const ASSETS = [
   "./",
   "./index.html",
